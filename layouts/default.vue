18801c9938c852f7b54d017bbc5383a1bcefa211
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2023-11-05 10:31:56 -0800

    cpyright year updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <script setup>
+const year = new Date().getFullYear()
 const navLinks = ref([
     { "title": 'Home', "link": "/" },
     { "title": 'Company', "link": "/company" },
@@ -132,7 +133,7 @@ onMounted(() => {
                 <div class="flex flex-wrap items-center md:justify-between justify-center">
                     <div class="w-full md:w-4/12 px-4 mx-auto text-center">
                         <div class="text-sm text-blueGray-500 font-semibold py-1">
-                            Copyright © <span id="get-current-year">2023</span><a href="#"
+                            Copyright © <span id="get-current-year">2016 - {{ year }}</span><a href="#"
                                 class="text-blueGray-500 hover:text-gray-800" target="_blank"> Digital Assets</a>
                         </div>
                     </div>
