a25a4fdda37866de63b39dffb3f4dc2b172a4d65
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2023-11-18 16:59:47 +0100

    Working on share Aholic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,28 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: "Digital Assets",
+      link: [
+        {
+          rel: 'preload',
+          href: 'https://cdn.shareaholic.net/assets/pub/shareaholic.js',
+          as: 'script'
+        },
+
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/img/logo.png'
+        }
+      ],
+
+      meta: [
+        {
+          name: 'shareaholic:site_id',
+          content: '833959d5dc1e891b01eaf8c799cc1602'
+        }
+      ],
+
+
       script: [
         { src: "https://s3.tradingview.com/tv.js", tagPosition: 'head' },
         {
@@ -16,6 +38,12 @@ export default defineNuxtConfig({
           tagPosition: 'bodyClose',
           async: 'true'
         },
+        {
+          hid: 'shareaholic-script',
+          src: 'https://cdn.shareaholic.net/assets/pub/shareaholic.js',
+          async: true,
+          defer: true // Optional: defer loading if necessary
+        }
       
       ],
     }
