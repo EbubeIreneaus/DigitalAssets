d479d8a91354b7e189ab3294b965d6688d9c3eb8
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2023-12-26 03:11:35 +0100

    transfer completed

--- components/modal.vue
+++ components/modal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="modal" class="fixed h-screen w-screen top-0 left-0 flex justify-center items-center z-50 bg-black/70"
+    <div id="modal" class="fixed h-screen w-screen top-0 left-0 flex justify-center items-center z-50 bg-black/70 rounded-lg"
     :class="{'hidden': !visible}">
         <div class="absolute z-50 max-w-sm sm:max-w-md md:max-w-lg w-full shadow-md  bg-white">
             <button class="float-right rounded-lg py-2 px-3 m-2 ring-1
